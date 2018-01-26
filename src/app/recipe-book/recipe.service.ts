e341be1c3896f7a7ccf282456f62e591b630cc26
Adel Acebal <adelacebal@yahoo.com>
2018-01-25 21:27:50 -0500

    ross component communication

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 @Injectable()
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe("Test Recipe", "This is simply a test", "http://iamafoodblog.com/wp-content/uploads/2017/09/korean-army-stew-8739w.jpg"),
