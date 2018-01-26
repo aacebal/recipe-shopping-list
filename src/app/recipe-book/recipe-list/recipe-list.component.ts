65f2bb36711d2b5c7d5818b84ba164fefe6f0d59
Adel Acebal <adelacebal@yahoo.com>
2018-01-25 21:18:48 -0500

    using recipe service

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe("Test Recipe", "This is simply a test", "http://iamafoodblog.com/wp-content/uploads/2017/09/korean-army-stew-8739w.jpg"),
-    new Recipe("Another Recipe", "This is simply a test", "http://iamafoodblog.com/wp-content/uploads/2017/09/korean-army-stew-8739w.jpg")
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -4,10 +4,14 @@ import { Recipe } from './recipe.model';
 @Injectable()
 export class RecipeService {
 
-  recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe("Test Recipe", "This is simply a test", "http://iamafoodblog.com/wp-content/uploads/2017/09/korean-army-stew-8739w.jpg"),
     new Recipe("Another Recipe", "This is simply a test", "http://iamafoodblog.com/wp-content/uploads/2017/09/korean-army-stew-8739w.jpg")
   ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
   constructor() { }
 
